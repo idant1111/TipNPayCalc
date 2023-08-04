c74e254f93d067d4cf01812414c7c2d9c4af7d50
Idan Teigfeld <idant1111@users.noreply.github.com>
2023-08-04 11:20:22 +0300

    Update SplitDisplay.js

--- SplitDisplay.js
+++ SplitDisplay.js
@@ -33,41 +33,4 @@ const SplitDisplay = ({ tableData, tipPercentage }) => {
   const { totalValue, tipAmount, totalAmountWithTip, peopleTotals } = calculateSplitAndTip();
 
   // Function to format currency with ₪ symbol
-  const formatCurrency = (value) => {
-    return value.toLocaleString('he-IL', { style: 'currency', currency: 'ILS' });
-  };
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.label}>Total Value: {formatCurrency(totalValue)}</Text>
-      <Text style={styles.label}>Tip Amount: {formatCurrency(tipAmount)}</Text>
-      <Text style={styles.label}>Total Amount with Tip: {formatCurrency(totalAmountWithTip)}</Text>
-
-      <Text style={styles.subTitle}>Split per Person:</Text>
-      {Object.entries(peopleTotals).map(([person, amount]) => (
-        <Text key={person} style={styles.label}>
-          {person}: {formatCurrency(amount)}
-        </Text>
-      ))}
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 20,
-    padding: 10,
-    backgroundColor: '#f0f0f0',
-  },
-  label: {
-    fontSize: 16,
-    marginBottom: 5,
-  },
-  subTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 5,
-  },
-});
-
-export default SplitDisplay;
+  const format
